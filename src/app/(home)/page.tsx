5f7d2099a369f91d6a21a6036eeb7eaaedc6e3de
firasel2 <rasel@lemonhive.com>
2024-11-23 18:21:36 +0600

    feat: add home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { FeatureCards } from "./sub-sections/feature-cards";
+import { Hero } from "./sub-sections/hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeatureCards />
+    </>
+  );
+}
---
+++ src/app/(home)/sub-sections/hero/index.tsx
@@ -0,0 +1,12 @@
+export function Hero() {
+  return (
+    <section className="container py-5">
+      <div className="w-full bg-purple-50 border border-solid border-purple-600 p-10 rounded-md">
+        <div className="flex flex-col gap-4 items-center justify-center min-h-[25rem]">
+          <h2 className="text-4xl font-bold">Home Page</h2>
+          <p className="text-xl text-purple-800 font-semibold">Static Page</p>
+        </div>
+      </div>
+    </section>
+  );
+}
